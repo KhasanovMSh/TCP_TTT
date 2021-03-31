c45d8a85f809b8df59fd136575aaf024b2196e65
Murat <muratkhasanov99@mail.ru>
2021-03-31 22:37:21 +0300

    4/5 of work

--- AppCSharp/AppCSharp/Form1.cs
+++ AppCSharp/AppCSharp/Form1.cs
@@ -137,6 +137,13 @@ namespace AppCSharp
                      else*/
                     if (message.Contains("Победили"))
                     {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                buttons[i, j].Click -= button1_Click;
+                            }
+                        }
                         MessageBox.Show(message);
                     }
                     if (message.Contains('|'))
